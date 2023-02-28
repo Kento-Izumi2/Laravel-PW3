d1d91bb95e4e163ebf2ecfb45c0642e44b2a9ae5
Kento_Izumi2 <amanda.batista71@etec.sp.gov.br>
2023-02-28 10:33:13 -0300

    first commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>contato</h1>
+    <a href='/produtos'>produtos</a>
+    <a href='/empresa'>empresa</a>
+    
+</body>
+</html>
